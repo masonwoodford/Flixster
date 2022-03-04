4e80eb5cd9f60c4e830eaac21a4ac13ae860a620
Mason Woodford <63433735+masonwoodford@users.noreply.github.com>
2022-03-03 16:49:28 -0800

    added scene transition

--- app/src/main/java/com/example/flixster/PopularMovieViewHolder.kt
+++ app/src/main/java/com/example/flixster/PopularMovieViewHolder.kt
@@ -1,30 +1,31 @@
 package com.example.flixster
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.view.View
 import android.widget.ImageView
+import androidx.core.app.ActivityOptionsCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
-class PopularMovieViewHolder (v: View, context: Context, movies: List<Movie>) :  RecyclerView.ViewHolder(v), View.OnClickListener {
+class PopularMovieViewHolder (v: View, private val context: Context, private val movies: List<Movie>) :  RecyclerView.ViewHolder(v), View.OnClickListener {
     private val ivMovieImage = itemView.findViewById<ImageView>(R.id.ivBackdrop)
-    private val contextCurr = context
-    private val movies = movies
 
     init {
         itemView.setOnClickListener(this)
     }
 
     fun bind(movie: Movie) {
-        Glide.with(contextCurr).load(movie.movieImageUrl).placeholder(R.drawable.placeholder).into(ivMovieImage)
+        Glide.with(context).load(movie.movieImageUrl).placeholder(R.drawable.placeholder).into(ivMovieImage)
     }
 
     override fun onClick(v2: View?) {
         val movie = movies[adapterPosition]
-        val intent = Intent(contextCurr, DetailActivity::class.java)
+        val intent = Intent(context, DetailActivity::class.java)
+        val options = ActivityOptionsCompat.makeSceneTransitionAnimation(itemView.context as Activity, ivMovieImage, "movie_image")
         intent.putExtra(MOVIE_EXTRA, movie)
-        contextCurr.startActivity(intent)
+        context.startActivity(intent)
     }
 
 
--- app/src/main/java/com/example/flixster/StandardMovieViewHolder.kt
+++ app/src/main/java/com/example/flixster/StandardMovieViewHolder.kt
@@ -1,19 +1,20 @@
 package com.example.flixster
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
-import android.content.Context
-import android.content.Intent
+import androidx.core.app.ActivityOptionsCompat
 import androidx.recyclerview.widget.RecyclerView
+import androidx.core.util.Pair
 import com.bumptech.glide.Glide
 
-class StandardMovieViewHolder(v: View, context: Context, movies: List<Movie>) :  RecyclerView.ViewHolder(v), View.OnClickListener {
+class StandardMovieViewHolder(v: View, private val context: Context, private val movies: List<Movie>) :  RecyclerView.ViewHolder(v), View.OnClickListener {
     private val ivMovieImage = itemView.findViewById<ImageView>(R.id.ivMovieImage)
     private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
     private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
-    private val contextCurr = context
-    private val movies = movies
 
     init {
         itemView.setOnClickListener(this)
@@ -22,13 +23,20 @@ class StandardMovieViewHolder(v: View, context: Context, movies: List<Movie>) : 
     fun bind(movie: Movie) {
         tvTitle.text = movie.title
         tvOverview.text = movie.overview
-        Glide.with(contextCurr).load(movie.movieImageUrl).placeholder(R.drawable.placeholder).into(ivMovieImage)
+        Glide.with(context).load(movie.movieImageUrl).placeholder(R.drawable.placeholder).into(ivMovieImage)
     }
 
     override fun onClick(v2: View?) {
         val movie = movies[adapterPosition]
-        val intent = Intent(contextCurr, DetailActivity::class.java)
+        val intent = Intent(context, DetailActivity::class.java)
+        val vMovieImage = itemView.findViewById<ImageView>(R.id.ivMovieImage) as View
+        val vTitle = itemView.findViewById<ImageView>(R.id.tvTitle) as View
+        val vOverview = itemView.findViewById<ImageView>(R.id.tvOverview) as View
+        val titlePair = Pair(vTitle, "movie_title")
+        val overviewPair = Pair(vOverview, "movie_overview")
+        val imagePair = Pair(vMovieImage, "movie_image")
+        val options = ActivityOptionsCompat.makeSceneTransitionAnimation(itemView.context as Activity, titlePair, overviewPair, imagePair)
         intent.putExtra(MOVIE_EXTRA, movie)
-        contextCurr.startActivity(intent)
+        context.startActivity(intent, options.toBundle())
     }
 }
